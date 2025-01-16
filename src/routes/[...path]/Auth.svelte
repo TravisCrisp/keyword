8049526fd96f7b9579b0e7bfeffbe51b33315085
Travis Crisp <46977795+TravisCrisp@users.noreply.github.com>
2025-01-16 16:50:17 -0700

    Added view password

--- src/routes/[...path]/Auth.svelte
+++ src/routes/[...path]/Auth.svelte
@@ -1,8 +1,14 @@
 <script>
 	import Container from "$lib/components/Container.svelte";
+	import Eye from "$lib/icons/Eye.svelte";
+	import EyeSlash from "$lib/icons/EyeSlash.svelte";
 
     let { page } = $props();
+    let showPassword = $state(false);
 
+    function togglePasswordVisibility() {
+        showPassword = !showPassword;
+    }
 </script>
 
 {#if page.path === '/sign-in'}
@@ -14,8 +20,15 @@
             </div>
             <div class="flex flex-col space-y-2">
                 <label for="password" class="font-bold">Password</label>
-                <input type="password" name="password" class="border border-gray-300 rounded-md p-2" />
+                <input type={showPassword ? "text" : "password"} name="password" class="border border-gray-300 rounded-md p-2" />
             </div>
+            <button type="button" onclick={togglePasswordVisibility} class="text-blue-500 max-w-fit">
+                {#if showPassword}
+                    <EyeSlash />
+                {:else}
+                    <Eye />
+                {/if}
+            </button>
             <button type="submit" class="bg-blue-500 text-white rounded-md p-2">Sign in</button>
         </form>
 </Container>
@@ -28,12 +41,19 @@
             </div>
             <div class="flex flex-col space-y-2">
                 <label for="password" class="font-bold">Password</label>
-                <input type="password" name="password" class="border border-gray-300 rounded-md p-2" required/>
+                <input type={showPassword ? "text" : "password"} name="password" class="border border-gray-300 rounded-md p-2" required/>
             </div>
             <div class="flex flex-col space-y-2">
                 <label for="password_confirm" class="font-bold">Confirm Password</label>
-                <input type="password" name="password_confirm" class="border border-gray-300 rounded-md p-2" required/>
+                <input type={showPassword ? "text" : "password"} name="password_confirm" class="border border-gray-300 rounded-md p-2" required/>
             </div>
+            <button type="button" onclick={togglePasswordVisibility} class="text-blue-500 max-w-fit">
+                {#if showPassword}
+                    <EyeSlash />
+                {:else}
+                    <Eye />
+                {/if}
+            </button>
             <div class="flex items-center space-x-2">   
                 <input type="checkbox" name="terms" id="terms" class="border border-gray-300 rounded-md p-2" required />
                 <label for="terms">I agree to the <a href="/terms-of-service" class="text-blue-500" target="_blank">terms and conditions</a></label>
